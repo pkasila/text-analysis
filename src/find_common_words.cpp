1a39a2c6dff609d17cda311a70dda90d556bfe76
Pavel Kasila <pavel.kasila@gmail.com>
2021-10-29 15:06:43 +0300

    #14: sort words

--- src/find_common_words.cpp
+++ src/find_common_words.cpp
@@ -7,11 +7,16 @@
 
 std::map<std::string, size_t> find_common_words(const std::string &str) {
     std::vector<std::string> words = split_words(str);
-    std::map<std::string, size_t> m {};
+    std::map<std::string, size_t> m{};
 
-    for (const std::string& s : words) {
+    for (const std::string &s: words) {
         m[s] = m[s] + 1;
     }
+
+    auto cmp = [](std::pair<std::string, size_t> const &a, std::pair<std::string, size_t> const &b) {
+        return a.second != b.second ? a.second < b.second : a.first < b.first;
+    };
+    std::sort(m.begin(), m.end(), cmp);
 
     return m;
 }
