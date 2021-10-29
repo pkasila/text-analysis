c0ddeecd368e6f9951b415452a314db2e12447fe
Pavel Kasila <pavel.kasila@gmail.com>
2021-10-29 12:51:04 +0300

    move base methods to utils, use count_char instead of count_alphabet with alphabet of one character

---
+++ src/utils.h
@@ -0,0 +1,15 @@
+//
+// Created by Pavel Kasila on 29.10.21.
+//
+
+#ifndef TEXT_ANALYSIS_UTILS_H
+#define TEXT_ANALYSIS_UTILS_H
+
+#include <string>
+#include <map>
+
+size_t count_alphabet(const std::string &str, const std::string &alphabet);
+
+size_t count_char(const std::string &str, char c);
+
+#endif //TEXT_ANALYSIS_UTILS_H
