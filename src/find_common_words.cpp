18f3f8748d32dcec6be26e6cfa44b017741ec397
Pavel Kasila <pavel.kasila@gmail.com>
2021-10-29 15:16:16 +0300

    #14: sorting didn't work, so currently removed

--- src/find_common_words.cpp
+++ src/find_common_words.cpp
@@ -2,6 +2,7 @@
 // Created by Pavel Kasila on 29.10.21.
 //
 
+#include <set>
 #include "find_common_words.h"
 #include "utils.h"
 
@@ -12,11 +13,6 @@ std::map<std::string, size_t> find_common_words(const std::string &str) {
     for (const std::string &s: words) {
         m[s] = m[s] + 1;
     }
-
-    auto cmp = [](std::pair<std::string, size_t> const &a, std::pair<std::string, size_t> const &b) {
-        return a.second != b.second ? a.second < b.second : a.first < b.first;
-    };
-    std::sort(m.begin(), m.end(), cmp);
 
     return m;
 }
--- src/find_common_words.h
+++ src/find_common_words.h
@@ -5,6 +5,7 @@
 #ifndef TEXT_ANALYSIS_FIND_COMMON_WORDS_H
 #define TEXT_ANALYSIS_FIND_COMMON_WORDS_H
 
+#include <algorithm>
 #include <map>
 
 std::map<std::string, size_t> find_common_words(const std::string &str);
