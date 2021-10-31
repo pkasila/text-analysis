4ef03594ee55fb8c3f1829734e6b15110cdb2d07
Pavel Kasila <pavel.kasila@gmail.com>
2021-10-31 20:12:08 +0300

    #12: revert to previous algo

--- src/three_letters_in_a_row.cpp
+++ src/three_letters_in_a_row.cpp
@@ -8,12 +8,12 @@
 size_t three_letters(const std::string &str) {
     int count = 0;
     std::vector <std::string> Words = split_words(str);
-    for(auto tem_str : Words){
+    for(auto tem_str : Words) {
         auto str_size = double(tem_str.size());
         int cur = 0;
-        while (cur <= str_size) {
+        while (cur != str_size) {
             int temp = 0;
-            while (is_vowel(tem_str[cur]) == is_vowel(tem_str[cur + temp]) && cur + temp <= str_size)
+            while (is_vowel(tem_str[cur]) == is_vowel(tem_str[cur + temp]) && cur + temp != str_size)
                 temp++;
             cur += temp;
             if (temp >= 3)
